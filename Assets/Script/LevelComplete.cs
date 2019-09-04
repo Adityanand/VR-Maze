728e281b9d9b5f6effb7da3c1c8842bef64e0b55
Aditya Anand <adityanand3003@gmail.com>
2019-09-04 22:31:18 +0530

    Initialization
    1st Commit

---
+++ Assets/Script/LevelComplete.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelComplete : MonoBehaviour {
+    public float Timer;
+    public float TimeConsume;
+    public GameObject StartPoint;
+    public GameObject EndPoint;
+    public bool TimerStart;
+    public bool LevelComp;
+    public Text TimerGUI;
+    public Text TimeTaken;
+    public GameObject LevelFailed;
+    public GameObject LevelCompleted;
+	// Use this for initialization
+	void Start () {
+        Timer = 300.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimerGUI.text = ("Timer:" + Mathf.Floor(Timer) + " Sec");
+		if(TimerStart==true)
+        {
+            Timer -= 1 * Time.deltaTime;
+        }
+        if(Timer<=1.0f)
+        {
+            GetComponent<Movement>().enabled = false;
+            LevelFailed.GetComponent<Canvas>().enabled = true;
+            TimerStart = false;
+        }
+        if((TimerStart==false) &&(LevelComp==true))
+        {
+            TimeConsume = 300 - Timer;
+            GetComponent<Movement>().enabled = false;
+            LevelCompleted.GetComponent<Canvas>().enabled = true;
+            TimeTaken.text = ("You get out of the maze in " + Mathf.Floor(TimeConsume)+" Seconds");
+        }
+	}
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == StartPoint)
+        {
+            TimerStart = true;
+        }
+        if(col.gameObject==EndPoint)
+        {
+            TimerStart = false;
+            LevelComp = true;
+        }
+    }
+}
---
+++ Assets/Script/SceneMAnger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMAnger : MonoBehaviour {
+
+	// Use this for initialization
+	public void Exit()
+    {
+        Application.Quit();
+	}
+	
+	// Update is called once per frame
+	public void TryAgain ()
+    {
+        SceneManager.LoadScene("VR Test");
+	}
+}
